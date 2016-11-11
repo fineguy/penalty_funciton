609b3f67558d2dc592f274c0f1d42b334c631434
Tim <timasem@gmail.com>
2016-11-12 02:56:51 +0800

    added new tests

--- test_penalty.py
+++ test_penalty.py
@@ -19,70 +19,51 @@ def alpha_gen(alpha0=1, beta=0.1):
         cur_val *= beta
 
 
-def solver(func, grad, x, eps, disp):
-    return minimize(func, x, method='CG', jac=grad, tol=eps)
+def solver(func, grad, x, disp):
+    return minimize(func, x, method='CG', jac=grad)
 
 
-def penalty_func(x):
-    pass
+def print_test(test_func):
+    def wrapper(*args, **kwargs):
+        print('---------- {} ----------'.format(test_func.__name__))
+        test_func(*args, **kwargs)
+    return wrapper
 
 
-def penalty_grad(x):
-    pass
-
-
-def output_result(res):
-    print("""
-    Finished optimization.
-    Final approximation:    {}
-    Final function value:   {:10f}
-    Final gradient norm:    {:10f}
-    -----------------------------------------------
-""".format(np.array_str(res.x), res.fun, np.linalg.norm(res.jac)))
-
-
+@print_test
 def test_simple():
     f = Function(
-        lambda x: x**2,
-        lambda x: x * 2
+        lambda x: x[0]**2,
+        lambda x: x[0] * 2
     )
     g = Function(
-        lambda x: x - 1,
-        lambda x: np.array([1])
+        lambda x: 1 - x[0],
+        lambda x: np.array([-1])
     )
-    P = Function(
-        lambda x: x**2,
-        lambda x: x * 2
-    )
-    x0 = np.array([10])
-
-    res = penalty_optim(f, [g], P, solver, x0, alpha_gen(), disp=False)
-    output_result(res)
+    x0 = -10
+    return penalty_optim(f, [g], [], solver, x0, alpha_gen(), disp=True)
 
 
+@print_test
 def test_medium():
     f = Function(
         lambda x: x[0]**2 + x[0] * 6 + x[1]**2 + x[1] * 9,
         lambda x: 2 * x + np.array([6, 9])
     )
     g1 = Function(
-        lambda x: x[0],
-        lambda x: np.array([1, 0])
+        lambda x: -x[0],
+        lambda x: np.array([-1, 0])
     )
     g2 = Function(
-        lambda x: x[1],
-        lambda x: np.array([0, 1])
-    )
-    P = Function(
-        lambda x: x**2,
-        lambda x: x * 2
+        lambda x: -x[1],
+        lambda x: np.array([0, -1])
     )
     x0 = np.array([-1, 0.5])
-
-    res = penalty_optim(f, [g1, g2], P, solver, x0, alpha_gen(), disp=True)
-    output_result(res)
+    return penalty_optim(f, [g1, g2], [], solver, x0, alpha_gen(), disp=True)
 
 
+'''
+@print_test
 def test_hard():
     f = Function(
         lambda x: x[0]**2 + x[1]**2 + 2 * x[2]**2 + x[3]**2 - 5 * x[0] - 5 * x[1] - 21 * x[2] + 7 * x[3],
@@ -120,64 +101,71 @@ def test_hard():
             1
         ])
     )
-    P = Function(
-        lambda x: x**2,
-        lambda x: x * 2
-    )
+
     x0 = np.zeros(4)
 
-    res = penalty_optim(f, [g1, g2, g3], P, solver, x0, alpha_gen(), disp=True)
-    output_result(res)
+    return penalty_optim(f, [g1, g2, g3], [], solver, x0, alpha_gen(), disp=True)
     # expect (0, 1, 2, -1) with function value -44
+'''
 
 
+@print_test
 def test_novak():
+    # expect (2.5, 2.5) with function value 4.5
     f = Function(
         lambda x: (x - 4).dot(x - 4),
         lambda x: 2 * (x - 4)
     )
-    g = Function(
-        lambda x: 5 - x.sum(),
-        lambda x: np.array([-1, -1])
+    g1 = Function(
+        lambda x: x[0] - 5,
+        lambda x: np.array([1, 0])
     )
-    P = Function(
-        lambda x: x**2,
-        lambda x: 2 * x
+    g2 = Function(
+        lambda x: x.sum() - 3,
+        lambda x: np.array([1, 1])
     )
     x0 = np.zeros(2)
-
-    res = penalty_optim(f, [g], P, solver, x0, alpha_gen(), disp=True)
-    output_result(res)
-    # expect (2.5, 2.5) with function value 4.5
+    return penalty_optim(f, [g1], [g2], solver, x0, alpha_gen(), disp=True)
 
 
+@print_test
 def test_what():
+    # expect to reach limit in function calls
     f = Function(
-        lambda x: x**9,
-        lambda x: 9 * x**8
+        lambda x: x[0]**9,
+        lambda x: 9 * x[0]**8
     )
     g = Function(
-        lambda x: x - 100000,
-        lambda x: 1
+        lambda x: 100000 - x[0],
+        lambda x: np.array([-1])
     )
-    P = Function(
-        lambda x: x**2,
-        lambda x: 2 * x
-    )
-    x0 = 0
+    x0 = np.array([0])
+    return penalty_optim(f, [g], [], solver, x0, alpha_gen(), disp=True)
 
-    res = penalty_optim(f, [g], P, solver, x0, alpha_gen(), disp=False)
-    output_result(res)
-    # expect to reach limit in function calls
+
+@print_test
+def test_impossible():
+    # expect to diverge
+    f = Function(
+        lambda x: np.exp(-x[0]),
+        lambda x: -np.exp(-x[0])
+    )
+    g = Function(
+        lambda x: x[0] * np.exp(-x[0]),
+        lambda x: (1 - x[0]) * np.exp(-x[0])
+    )
+    x0 = np.array([1])
+    return penalty_optim(f, [], [g], solver, x0, alpha_gen(), eps=1e-20, disp=True)
 
 
 def main():
     np.set_printoptions(precision=8, suppress=True)
     # test_simple()
-    test_medium()
+    # test_medium()
     # test_hard()
     # test_novak()
     # test_what()
+    test_impossible()
 
 
 if __name__ == '__main__':
